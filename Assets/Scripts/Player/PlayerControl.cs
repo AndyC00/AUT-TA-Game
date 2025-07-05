524ba8dc87fe62d68f9498049e5389a068b750d7
AndyC00 <chenjinzhi96@gmail.com>
2025-07-05 19:28:39 +1200

    player control action map added

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -1,18 +1,44 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerControl : MonoBehaviour
 {
-    void Start()
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 6f;
+
+    private Rigidbody2D rb;
+    private PlayerControls controls;
+
+    private float moveInput;     // ?? X?-1 ~ 1?
+    private bool jumpQueued;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        // ????????
+        controls = new PlayerControls();
+
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>().x;
+        controls.Player.Move.canceled += _ => moveInput = 0f;
+
+        controls.Player.Jump.performed += _ => jumpQueued = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable() => controls.Player.Enable();
+    void OnDisable() => controls.Player.Disable();
+
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        { 
-            //Jump
+        // ? ??????????????
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // ? ??????????“y ???0 ?????”?
+        if (jumpQueued && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+        jumpQueued = false;   // ??????
     }
 }
