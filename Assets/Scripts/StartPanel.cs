d664d9f6514ce2fa36e12dce1c76e7e257ec9bc4
AndyC00 <chenjinzhi96@gmail.com>
2025-07-05 17:55:59 +1200

    start panel added

---
+++ Assets/Scripts/StartPanel.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StartPanel : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()   // press Enter to hide this panel
+    {
+
+        if (Keyboard.current == null) { Debug.Log("keyboard doesn't applied!"); return; }
+
+        if (Keyboard.current.enterKey.wasPressedThisFrame || Keyboard.current.numpadEnterKey.wasPressedThisFrame)
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
