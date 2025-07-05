1aad02eed0e2a451fa6fd64e8f62b7452bc7bd10
AndyC00 <chenjinzhi96@gmail.com>
2025-07-05 18:36:24 +1200

    rb, collider added, start on player control

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            //Jump
+        }
+    }
+}
